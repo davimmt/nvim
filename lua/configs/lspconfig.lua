2a2f080fd05f05db4b8ec84ea628c5295aa2cc87
Davi Tavares <mirandadavi.mt@gmail.com>
2024-03-30 00:44:39 -0300

    feat(lsp): yamlls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = {}
+local servers = {"yamlls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
